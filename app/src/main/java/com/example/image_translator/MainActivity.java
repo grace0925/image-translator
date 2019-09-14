3a4569bf5f4cf4cbd5ca1d3d1a0a2058473d2a04
GraceLiu <graceliu0925@gmail.com>
2019-09-14 11:36:34 -0400

    🍆 Literally nothing test

--- app/src/main/java/com/example/image_translator/MainActivity.java
+++ app/src/main/java/com/example/image_translator/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.image_translator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testBtn = (Button) findViewById(R.id.button);
+
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Grace is here!!!!!!!!!!!!!");
+            }
+        });
     }
 }
